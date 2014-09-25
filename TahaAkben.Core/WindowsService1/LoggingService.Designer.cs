81e681085743a27abf68104e649aead48557b896
akben <akben.taha@gmail.com>
2014-09-25 09:58:16 +0300

    commit edelim mi ?

---
+++ TahaAkben.Core/WindowsService1/LoggingService.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace WindowsService1
+{
+    partial class LoggingService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3600000;
+            // 
+            // Logging
+            // 
+            this.ServiceName = "Service1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
